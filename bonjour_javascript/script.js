295443a37a19ca6c56c517da393f20526097601f
Werner <werner.wolff1109@gmail.com>
2025-06-10 11:00:39 +0200

    Ajout du second paramètre et réecriture du code

--- bonjour_javascript/script.js
+++ bonjour_javascript/script.js
@@ -1,4 +1,6 @@
 
+/* Étape 2 : premier code
+
 //let message = "Bonjour"
 //console.log(message)
 // on vérifie que la variable message nous rends bien le bon message
@@ -6,12 +8,33 @@
 //let firstName = "Beyonce"
 //message = `Bonjour ${firstName} !` // changement de la variable de message cf. énoncé
 //console.log(message)
+*/
 
-function sayHello(firstName){
+/* Étape 3 : dans une fonction
+
+function sayHello(name){ // je change le nom de la variable pour qu'on puisse l'appeler sans que ça ne bug
 
     let message = "Bonjour"
+    let firstName = "Beyonce"
     message = `Bonjour ${firstName} !`
     console.log(message)
 }
 //sayHello(firstName)
+
 sayHello() // l'appel de fonction permet de re afficher "Bonjour Beyonce !"
+
+*/
+
+// Étape 4 : un second paramètre
+// j'ai bien réussi le faire de cette façon mais je n'ai pas réutiliser les variables définies plus haut
+function sayHello(name, hour){
+    if (hour < 18 ){
+        console.log(`Bonjour ${name} !`)
+    }  else {
+        console.log(`Bonsoir ${name} !`)
+    }
+}
+
+sayHello("Beyonce", 11)
+sayHello(`Beyonce`, 18)
+sayHello(`Beyonce`, 17)
