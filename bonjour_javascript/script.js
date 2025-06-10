bcbedadc788221f864b9e40e440d0926e02224f7
Werner <werner.wolff1109@gmail.com>
2025-06-10 10:40:54 +0200

    création de la fonction sayHello et encapsulation des variables dans la fonction

--- bonjour_javascript/script.js
+++ bonjour_javascript/script.js
@@ -1,8 +1,20 @@
 
-let message = "Bonjour"
+//let message = "Bonjour"
 //console.log(message)
 // on vérifie que la variable message nous rends bien le bon message
 
-let firstName = "Beyonce"
-message = `Bonjour ${firstName} !` // changement de la variable de message cf. énoncé
-console.log(message)
+//let firstName = "Beyonce"
+//message = `Bonjour ${firstName} !` // changement de la variable de message cf. énoncé
+//console.log(message)
+
+function sayHello(){
+
+    let message = "Bonjour"
+    let firstName = "Beyonce"
+    message = `Bonjour ${firstName} !`
+    console.log(message)
+}
+//sayHello(firstName)
+// je n'ai pas réussi à avoir le message "Bonjour undefined !" mais "ReferenceError: firstName is not defined"
+
+sayHello() // l'appel de fonction permet de re afficher "Bonjour Beyonce !"
