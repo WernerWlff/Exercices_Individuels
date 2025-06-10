28f03f1018426186ffd7d267ec11c3714a314331
Werner <werner.wolff1109@gmail.com>
2025-06-10 10:17:32 +0200

    Changement de la valeur de message et ajout de la variable firstName

--- bonjour_javascript/script.js
+++ bonjour_javascript/script.js
@@ -1,4 +1,8 @@
 
-let message = "Bonjour !"
+let message = "Bonjour"
 //console.log(message)
 // on vérifie que la variable message nous rends bien le bon message
+
+let firstName = "Beyonce"
+message = `Bonjour ${firstName} !` // changement de la variable de message cf. énoncé
+console.log(message)
