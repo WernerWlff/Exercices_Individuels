7a630ae478f49f04962d4c76bc0a75a3894ebc03
Werner <werner.wolff1109@gmail.com>
2025-07-10 10:03:34 +0200

    Ajout étape 4 ( pas encore fonctionnel)

--- 5.Parlez_vous_le_morse/script.js
+++ 5.Parlez_vous_le_morse/script.js
@@ -12,7 +12,7 @@ function translateLatinCharacter(letter){
 }
 
 function encode(){
-    let message = prompt('Saissisez le message que vous voulez traduire en morse ?')
+    let message = prompt('Saissisez le message que vous voulez traduire en morse')
     let traductionInMorse = getLatinCharacterList(message)
     //console.log(traductionInMorse) //---> à ce moment la, on récupère le tableau de notre message
     let messageInMorse = ""
@@ -32,4 +32,39 @@ function encode(){
 
 }
 
-encode()
+//encode()
+
+
+//------------------------------------------------------------Morse --> String-----------------------------------------------------//
+
+//On récupère notre message
+function getMorseCharacterList(morseToChange){
+    let morseToArray = morseToChange.split('')
+    return morseToArray
+}
+
+//On transforme le message
+function translateMorseCharacter(symbols){
+    return morseToLatin[symbols]
+}
+
+function decode(){
+    let morseMessage = prompt('Saissisez le message en morse que vous voulez traduire en latin')
+    let traductionInLatin = getMorseCharacterList(morseMessage)
+    let messageInLatin = ""
+
+    //on va transformer notre code en morse en lettres
+    for(let i = 0; i < traductionInLatin.length; i++){
+        traductionInLatin[i] = translateMorseCharacter(traductionInLatin[i])
+        console.log(traductionInLatin[i])
+    }
+
+    for(let j = 0; j < traductionInLatin.length; j++){
+        messageInLatin = messageInLatin + traductionInLatin[j]
+    }
+
+    console.log(messageInLatin)
+
+}
+
+decode()
