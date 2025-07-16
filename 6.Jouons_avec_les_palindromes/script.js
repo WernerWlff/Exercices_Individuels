73cbc847e75c2ead9d6a637d90f64057a6d068f0
Werner <werner.wolff1109@gmail.com>
2025-07-16 12:32:03 +0200

    Étape 1 exercice individuel 6

---
+++ 6.Jouons_avec_les_palindromes/script.js
@@ -0,0 +1,69 @@
+function isValidDate(date){
+    let arrayOfString = date.split('/') // sépare notre date en tableau de string
+
+    let dd = arrayOfString[0]
+    let mm = arrayOfString[1]
+    let yyyy = arrayOfString[2]
+
+
+    if (yyyy >= 1000 && yyyy <= 9999){
+        maxDaysInMonth(dd, mm)
+    }
+    else {
+        console.log("La date n'est pas valide car l'année n'est pas prise en charge")
+    }
+
+}
+
+function maxDaysInMonth(dayInMonth, month){
+
+    // on définit nos mois pour savoir combien de jours on a dans notre mois
+    const MONTH31 = ["01", "03", "05", "07", "08", "10", "12"]
+    const MONTH30 = ["04", "06", "09", "11"]
+    const FEVRIER = ["02"]
+
+    if(month >= "01" && month <= "12"){
+        // c'est des conditions à part
+        if(dayInMonth >= "01" && dayInMonth <= "28"){
+                if(FEVRIER.includes(month)){
+                    console.log("la date est valide")
+                    return true
+                }
+        }
+
+        // c'est des conditions à part
+        if(dayInMonth >= "01" && dayInMonth <= "31"){
+            if(MONTH31.includes(month)){ // on vérifie que c'est bien un mois de 31 jours
+                console.log("la date est valide")
+                return true
+            }
+        }
+
+        // c'est des conditions à part
+        if(dayInMonth >= "01" && dayInMonth <= "30"){
+                if(MONTH30.includes(month)){ // on vérifie que c'est bien un mois de 30 jours
+                    console.log("la date est valide")
+                    return true
+                }
+        }
+
+        console.log("la date n'est pas valide car le jour n'est pas bon")
+        return false
+    }
+
+    else{
+        console.log("la date n'est pas valide car le mois n'est pas bon")
+    }
+
+}
+isValidDate("31/01/2001")
+isValidDate("30/11/2001")
+isValidDate("28/02/2001")
+
+isValidDate("31/02/2001")
+isValidDate("45/07/2001")
+isValidDate("31/11/2001")
+
+isValidDate("01/01/01")
+isValidDate("01/14/100000")
+isValidDate("15/14/2001")
