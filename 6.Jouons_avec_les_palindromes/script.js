fa804bff064645cc8c074ac92e1631c2eed12410
Werner <werner.wolff1109@gmail.com>
2025-07-18 14:36:26 +0200

    ajout commentaires + tests

--- 6.Jouons_avec_les_palindromes/script.js
+++ 6.Jouons_avec_les_palindromes/script.js
@@ -1,93 +1,3 @@
-function isValidDate(date) {
-  let arrayOfString = date.split("/"); // sépare notre date en tableau de string
-
-  let dd = arrayOfString[0];
-  let mm = arrayOfString[1];
-  let yyyy = arrayOfString[2];
-
-  if (yyyy >= 1000 && yyyy <= 9999 && maxDaysInMonth(dd, mm, yyyy) === true) {
-    return true
-  }
-
-  else{
-    return false
-  }
-}
-
-
-function maxDaysInMonth(dayInMonth, month, year) {
-  // on définit nos mois pour savoir combien de jours on a dans notre mois
-  const MONTH31 = ["01", "03", "05", "07", "08", "10", "12"];
-  const MONTH30 = ["04", "06", "09", "11"];
-  const FEVRIER = ["02"];
-  
-  // c'est des conditions à part
-  if (month >= "01" && month <= "12") {
-    if (dayInMonth >= "01" && dayInMonth <= "28") {
-      if (FEVRIER.includes(month)) {
-        return true;
-      }
-    }
-
-    // c'est des conditions à part
-    if (dayInMonth >= "01" && dayInMonth <= "31") {
-      if (MONTH31.includes(month)) {
-        // on vérifie que c'est bien un mois de 31 jours
-        return true;
-      }
-    }
-
-    // c'est des conditions à part
-    if (dayInMonth >= "01" && dayInMonth <= "30") {
-      if (MONTH30.includes(month)) {
-        // on vérifie que c'est bien un mois de 30 jours
-        return true;
-      }
-    }
-
-    // on vérifie si c'est une année bissextile 
-    if(month === "02" && dayInMonth === "29" && year % 4 == 0){
-      return true
-
-    }
-    return false;
-  }
-  
-  else {
-    return false;
-  }
-}
-
-function isPalindrome(string) {
-  let reverseString = "";
-
-  for (i = string.length - 1; i >= 0; i--) {
-    reverseString += string[i];
-  }
-
-  if (string === reverseString) {
-    return true;
-  }
-
-  else {
-    return false;
-  }
-}
-
-function isDatePalindrome(day, month, year) {
-  let datePalindrome = day + "/" + month + "/" + year;
-
-    if(isValidDate(datePalindrome) === true){
-      datePalindrome = day + month + year
-      if(isPalindrome(datePalindrome)=== true){
-        return true
-      }
-    }
-    else {
-      return false
-    }
-}
-
 function getNextPalindrome(numOfPalToFind) {
 
   //créer un élement date
@@ -123,6 +33,112 @@ function getNextPalindrome(numOfPalToFind) {
   }
 }
 
+
+//on va d'abord vérifier si notre date est valide
+function isDatePalindrome(day, month, year) {
+
+  //on recompose notre date pour faire la vérification de la date
+  let datePalindrome = day + "/" + month + "/" + year;
+
+    // si elle est valide on récréer encore une fois notre string au format ou elle pourra être retournée
+    if(isValidDate(datePalindrome) === true){
+      datePalindrome = day + month + year
+      
+      // on va vérifier si notre date est un palindrome
+      if(isPalindrome(datePalindrome)=== true){
+        return true
+      }
+    }
+    else {
+      return false
+    }
+}
+
+
+function isValidDate(date) {
+  let arrayOfString = date.split("/"); // sépare notre date en tableau de string
+
+  let dd = arrayOfString[0];
+  let mm = arrayOfString[1];
+  let yyyy = arrayOfString[2];
+
+  if (yyyy >= 1000 && yyyy <= 9999 && maxDaysInMonth(dd, mm, yyyy) === true) {
+    return true
+  }
+
+  else{
+    return false
+  }
+}
+
+
+function maxDaysInMonth(dayInMonth, month, year) {
+  // on définit nos mois pour savoir combien de jours on a dans notre mois
+  // on créer ces tableaux pour ensuite les comparer avec les valeurs données pour savoir si le jour donné est possible
+  const MONTH31 = ["01", "03", "05", "07", "08", "10", "12"];
+  const MONTH30 = ["04", "06", "09", "11"];
+  const FEVRIER = ["02"];
+  
+  // c'est des conditions à part
+  if (month >= "01" && month <= "12") {
+    if (dayInMonth >= "01" && dayInMonth <= "28") {
+      if (FEVRIER.includes(month)) {
+        return true;
+      }
+    }
+
+    // c'est des conditions à part
+    if (dayInMonth >= "01" && dayInMonth <= "31") {
+      if (MONTH31.includes(month)) {
+        // on vérifie que c'est bien un mois de 31 jours
+        return true;
+      }
+    }
+
+    // c'est des conditions à part
+    if (dayInMonth >= "01" && dayInMonth <= "30") {
+      if (MONTH30.includes(month)) {
+        // on vérifie que c'est bien un mois de 30 jours
+        return true;
+      }
+    }
+
+    // on vérifie si c'est une année bissextile 
+    if(month === "02" && dayInMonth === "29" && year % 4 == 0){
+      return true
+
+    }
+    //permet de s'assurer d'avoir un résultat si aucune condition n'est respectée
+    return false;
+  }
+  
+  //nous renvoi faux si le mois n'est pas inclus
+  else {
+    return false;
+  }
+}
+
+
+//on créer une string inversée et la comparer avec notre string de base
+function isPalindrome(string) {
+  let reverseString = "";
+
+  for (i = string.length - 1; i >= 0; i--) {
+    reverseString += string[i];
+  }
+
+  if (string === reverseString) {
+    return true;
+  }
+
+  else {
+    return false;
+  }
+}
+
+
+
 getNextPalindrome(8);
 // console.log(isPalindrome("kayak"))
+// console.log(isPalindrome("yakak"))
 // console.log(isValidDate("29/02/2091"))
