a8e20f39e417cdb6fbf2d67e185aa033aacde9f1
Werner Wolff <werner.wolff1109@gmail.com>
2025-09-15 15:42:27 +0200

    chore: clean code

--- 9.Sapin/sapin.js
+++ 9.Sapin/sapin.js
@@ -1,45 +1,4 @@
-// function afficherPointeSapin(hauteur){
-//     let finalTree = "+"
-//     for (let i = 0; i < hauteur; i++){
-
-//     }
-// }
-
 let finalTree = ""
-
-function afficherEtoiles(n){
-    let totalStars = "*".repeat(n)
-    console.log(totalStars)
-}
-
-// afficherEtoiles(4)
-// afficherEtoiles(1000)
-
-function afficherRectangle(hauteur, largeur){
-    for(let i = 0; i < hauteur; i++){
-        afficherEtoiles(largeur)
-    }
-}
-
-// afficherRectangle(5,5)
-
-function afficherTriangleGauche(n){
-    for(let i = 0; i < n; i++){
-        finalTree += " ".repeat(n - i - 1) + "/" + "*".repeat(i) + "\n"
-    }
-    console.log(finalTree)
-}
-
-// afficherTriangleGauche(5)
-
-function afficherTriangleDroite(n){
-    for(let i = 0; i < n; i++){
-        finalTree += "*".repeat(i) + "\\" + "\n"
-    }
-    console.log(finalTree)
-}
-
-// afficherTriangleDroite(5)
 
 function afficherPointeSapin(largeur){
     finalTree = " ".repeat(largeur) + "+" + "\n"
